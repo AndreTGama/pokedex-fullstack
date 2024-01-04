defe8c3b136d588d960ed43a1090c3a73835b21e
AndreTGama <andre_gama789@hotmail.com>
2024-01-04 14:08:16 -0300

    altered docker-compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        host: true,
+        port: 3001,
+        watch: {
+            usePolling: true,
+        },
+    },
+});
